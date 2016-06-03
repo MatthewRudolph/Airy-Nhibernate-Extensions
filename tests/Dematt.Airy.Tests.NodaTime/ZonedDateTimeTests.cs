7da45cc730c38b132209ebcf2e9677646dcdba0c
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2016-06-03 16:31:43 +0100

    More tests for ZonedDateTimeType.

--- tests/Dematt.Airy.Tests.NodaTime/ZonedDateTimeTests.cs
+++ tests/Dematt.Airy.Tests.NodaTime/ZonedDateTimeTests.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using System.Linq;
 using Dematt.Airy.Nhibernate.NodaTime;
 using Dematt.Airy.Tests.NodaTime.Entities;
 using NHibernate;
+using NHibernate.Linq;
+using NHibernate.Util;
 using NodaTime;
 using NUnit.Framework;
 
@@ -37,7 +39,6 @@ namespace Dematt.Airy.Tests.NodaTime
             }
         }
 
-
         /// <summary>
         /// Can we write and read a ZonedDateTime stored as a DateTimeOffset and a string for the DateTimeZone Id.
         /// </summary>
@@ -47,13 +48,13 @@ namespace Dematt.Airy.Tests.NodaTime
             var timeZone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
             Instant now = SystemClock.Instance.Now;
             ZonedDateTime startZonedDateTime = now.InZone(timeZone);
-            ZonedDateTime finihshZonedDateTime = startZonedDateTime.Plus(Duration.FromHours(1));
+            ZonedDateTime finishZonedDateTime = startZonedDateTime.Plus(Duration.FromHours(1));
 
             var testEvent = new ZonedDateTimeTestEntity
             {
-                Description = "Can_Write_And_Read_OffsetDateTime_Stored_As_DateTimeOffset",
+                Description = "Can_Write_And_Read_ZonedDateTime_Stored_As_DateTimeOffset",
                 StartZonedDateTime = startZonedDateTime,
-                FinishZonedDateTime = finihshZonedDateTime
+                FinishZonedDateTime = finishZonedDateTime
             };
 
             using (ISession session = SessionFactory.OpenSession())
@@ -74,6 +75,81 @@ namespace Dematt.Airy.Tests.NodaTime
             Assert.That(retrievedEvent.StartZonedDateTime, Is.EqualTo(testEvent.StartZonedDateTime));
             Assert.That(retrievedEvent.FinishZonedDateTime, Is.EqualTo(testEvent.FinishZonedDateTime));
             Assert.That(retrievedEvent, Is.EqualTo(testEvent));
+        }
+
+        /// <summary>
+        /// Can we write and read a ZonedDateTime with a null value stored as a DateTimeOffset and a string for the DateTimeZone Id.
+        /// </summary>
+        [Test]
+        public void Can_Write_And_Read_ZonedDateTimeNullable_Stored_As_DateTimeOffset()
+        {
+            var timeZone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
+            Instant now = SystemClock.Instance.Now;
+            ZonedDateTime startZonedDateTime = now.InZone(timeZone);
+
+            var testEvent = new ZonedDateTimeTestEntity
+            {
+                Description = "Can_Write_And_Read_ZonedDateTimeNullable_Stored_As_DateTimeOffset",
+                StartZonedDateTime = startZonedDateTime,
+                FinishZonedDateTime = null
+            };
+
+            using (ISession session = SessionFactory.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(testEvent);
+                transaction.Commit();
+            }
+
+            ZonedDateTimeTestEntity retrievedEvent;
+            using (ISession session = SessionFactory.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                retrievedEvent = session.Get<ZonedDateTimeTestEntity>(testEvent.Id);
+                transaction.Commit();
+            }
+
+            Assert.That(retrievedEvent.StartZonedDateTime, Is.EqualTo(testEvent.StartZonedDateTime));
+            Assert.That(retrievedEvent.FinishZonedDateTime, Is.EqualTo(null));
+            Assert.That(retrievedEvent, Is.EqualTo(testEvent));
+        }
+
+        /// <summary>
+        /// Can we query by equals a ZonedDateTime stored as DateTimeOffset and a string for the DateTimeZone Id.
+        /// </summary>
+        [Test]
+        public void Can_Query_By_Equals_ZonedDateTime_Stored_As_DateTimeOffset()
+        {
+            var timeZone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
+            Instant now = SystemClock.Instance.Now;
+            ZonedDateTime startZonedDateTime = now.InZone(timeZone);
+            ZonedDateTime finishZonedDateTime = startZonedDateTime.Plus(Duration.FromHours(1));
+
+            var testEvent = new ZonedDateTimeTestEntity
+            {
+                Description = "Can_Query_By_Equals_ZonedDateTime_Stored_As_DateTimeOffset",
+                StartZonedDateTime = startZonedDateTime,
+                FinishZonedDateTime = finishZonedDateTime
+            };
+
+
+            using (ISession session = SessionFactory.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(testEvent);
+                transaction.Commit();
+            }
+
+            var searchTime = new ZonedDateTime(finishZonedDateTime.ToInstant(), finishZonedDateTime.Zone);
+            using (ISession session = SessionFactory.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                var query = session.Query<ZonedDateTimeTestEntity>().Where(x => x.FinishZonedDateTime == searchTime);
+                var retrievedEvent = query.SingleOrDefault();
+                transaction.Commit();
+                Assert.That(testEvent, Is.Not.Null);
+                Assert.That(testEvent, Is.EqualTo(retrievedEvent));
+            }
         }
     }
 }
