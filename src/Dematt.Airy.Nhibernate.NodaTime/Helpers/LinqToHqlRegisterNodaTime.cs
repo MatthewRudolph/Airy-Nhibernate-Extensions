eb241997410bd4c958e29b27d78b0cfeb50cd129
Matthew Rudolph <Matthew.Rudolph@users.noreply.github.com>
2016-08-01 14:29:30 +0100

    Rename the LinqToHqlGeneratorsRegistry class to LinqToHqlRegisterNodaTime.

---
+++ src/Dematt.Airy.Nhibernate.NodaTime/Helpers/LinqToHqlRegisterNodaTime.cs
@@ -0,0 +1,15 @@
+﻿
+
+using NHibernate.Linq.Functions;
+
+namespace Dematt.Airy.Nhibernate.NodaTime.Helpers
+{
+    // Use configuration.LinqToHqlGeneratorsRegistry<LinqToHqlRegisterNodaTime>() to register this
+    public class LinqToHqlRegisterNodaTime : DefaultLinqToHqlGeneratorsRegistry
+    {
+        public LinqToHqlRegisterNodaTime()
+        {
+            this.Merge(new ZonedDateTimeGtLtGenerator());
+        }
+    }
+}
